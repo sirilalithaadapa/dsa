a52d78ad639d80706149ed6cc3dc78cbf75b15b5
Siri Lalitha <19bq1a05l4@vvit.net>
2021-05-19 14:12:44 +0530

    Prerequisites: Problem 4: Cycle detection solution (Hacker rank)

---
+++ Hacker Rank/CycleDetection.java
@@ -0,0 +1,85 @@
+// Author :- Siri Lalitha Adapa
+
+// Problem statement: https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cycle/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the hasCycle function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        Set<SinglyLinkedListNode> seen = new HashSet<>();
+        SinglyLinkedListNode curr = head;
+        
+        while (curr != null) {
+            if (seen.contains(curr)) {
+                return true;
+            } else {
+                seen.add(curr);
+                curr = curr.next;
+            }
+        }
+        return false;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
